29eaa0c699e0daf2fe71f61a7e30760c954044a7
gaabs <gaabs@cin.ufpe.br>
2016-12-11 19:06:04 -0300

    Basic mob hit visual feedback

--- Assets/Script/MobGolem.cs
+++ Assets/Script/MobGolem.cs
@@ -10,29 +10,22 @@ public class MobGolem : Mob {
 		golemNavScript = GetComponent<SimpleNavScript> ();
 	}
 
-	override
-	public void takeDamage(float damage){
-		health -= damage;
-		//print (gameObject.name + " newHealth:" + health);
+	override public void Die(){
+		Vector3 position = transform.position;
+		Vector3 randomVector1 = Random.insideUnitSphere;
+		Vector3 randomVector2 = Random.insideUnitSphere;
+		randomVector1.y = randomVector2.y = 0;
 
-		if (health <= 0) {
-			//TODO: fix spawn position
-			Vector3 position = transform.position;
-			Vector3 randomVector1 = Random.insideUnitSphere;
-			Vector3 randomVector2 = Random.insideUnitSphere;
-			randomVector1.y = randomVector2.y = 0;
-
-			GameObject spawnedObject1 = Instantiate (miniGolem, position + randomVector1, Quaternion.identity) as GameObject;
-			GameObject spawnedObject2 = Instantiate (miniGolem, position + randomVector2, Quaternion.identity) as GameObject;
-			//spawnedObject.GetComponentInChildren<Rigidbody> ().transform = position + randomVector1;
-			UpdateNavScript(spawnedObject1);
-			NetworkServer.Spawn(spawnedObject1);
-			UpdateNavScript(spawnedObject2);
-			//spawnedObject.GetComponentInChildren<Rigidbody> ().transform = position + randomVector2;
-			NetworkServer.Spawn(spawnedObject2);
-			GameManager.instance.countMobKilled ();
-			Destroy (gameObject);
-		}
+		GameObject spawnedObject1 = Instantiate (miniGolem, position + randomVector1, Quaternion.identity) as GameObject;
+		GameObject spawnedObject2 = Instantiate (miniGolem, position + randomVector2, Quaternion.identity) as GameObject;
+		//spawnedObject.GetComponentInChildren<Rigidbody> ().transform = position + randomVector1;
+		UpdateNavScript(spawnedObject1);
+		NetworkServer.Spawn(spawnedObject1);
+		UpdateNavScript(spawnedObject2);
+		//spawnedObject.GetComponentInChildren<Rigidbody> ().transform = position + randomVector2;
+		NetworkServer.Spawn(spawnedObject2);
+		GameManager.instance.countMobKilled ();
+		Destroy (gameObject);
 	}
 
 	private void UpdateNavScript(GameObject miniGolem){
