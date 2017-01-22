4becef49521aa70394944936784988093d19670b
deyvson lazaro <deyvson lazaro>
2017-01-22 02:33:19 -0200

    player weapon adjust

---
+++ Assets/Script/WeaponScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+
+	    if (other.tag == "Mob")
+	    {
+	        other.GetComponent<Mob>().takeDamage(damage);
+	    }
+	}
+
+	void OnCollisionEnter(Collision collision){
+		print ("collision");
+
+	}
+}
