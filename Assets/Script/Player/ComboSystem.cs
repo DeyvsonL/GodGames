47171ead545fabb7432bc311be9339ce4be17c02
gaabs <gaabs@cin.ufpe.br>
2017-01-22 11:55:01 -0300

    Add dash

---
+++ Assets/Script/Player/ComboSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboSystem {
+
+	public KeyCode[]keys;
+	public int index;
+	public float inBetweenTime;
+	public float lastKeyPressTime;
+
+	public ComboSystem (KeyCode[] k)
+	{
+		index=0;
+		inBetweenTime=0.5f;
+		lastKeyPressTime=0.0f;
+		keys=k;
+	}
+
+	public bool Check()
+	{
+		if ( Time.time > lastKeyPressTime+inBetweenTime)
+		{
+			index=0;
+			lastKeyPressTime=Time.time;
+			return false;
+		}
+		else
+		{
+			if(index<keys.Length)
+			{
+				if(Input.GetKeyDown(keys[index]))
+				{
+					lastKeyPressTime=Time.time;
+					index++;
+					if(index>=keys.Length)
+					{
+						index=0;
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -31,6 +31,17 @@ public class PlayerMovement : MonoBehaviour {
     public AudioClip soundJump;
     public float volSoundJump;
     private AudioSource source;
+	
+	private ComboSystem leftDashCombo = new ComboSystem(new KeyCode[] {KeyCode.A, KeyCode.A});
+	private ComboSystem rightDashCombo = new ComboSystem(new KeyCode[] {KeyCode.D, KeyCode.D});
+	private ComboSystem frontDashCombo = new ComboSystem(new KeyCode[] {KeyCode.W, KeyCode.W});
+	private ComboSystem backDashCombo = new ComboSystem(new KeyCode[] {KeyCode.S, KeyCode.S});
+	private bool dash;
+	public float dashDistance = 0.5f;
+	public float dashSpeed = 10f;
+	public float dashDuration = 1f;
+	private float dashCurrentTime = 0f;
+	private Vector3 dashDirection;
 
     void Start() {
         source = GetComponent<AudioSource>();
@@ -48,28 +59,94 @@ public class PlayerMovement : MonoBehaviour {
 		if (player.Dead || gameManager.Win)
 			return;
 		
+		// Jump
 		grounded  = Physics.Raycast(transform.position + transform.forward * 0.45f, -transform.up, 1.4f) 
 			|| Physics.Raycast(transform.position - transform.forward * 0.45f, -transform.up, 1.4f) 
 			|| Physics.Raycast(transform.position + transform.right * 0.45f, -transform.up, 1.4f) 
 			|| Physics.Raycast(transform.position - transform.right * 0.45f, -transform.up, 1.4f) ;
 
-		movementInput = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        anim.SetFloat("Horizontal", movementInput.x);
-        anim.SetFloat("Vertical", movementInput.y);
         jumpInput = Input.GetButtonDown ("Jump") && grounded;
+		
         if (jumpInput)
         {
             anim.SetTrigger("Jump");
             source.PlayOneShot(soundJump, volSoundJump);
         }
+		
+		// Move
+		movementInput = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+			
+		// Dash
+		if (leftDashCombo.Check ()) {
+			dash = true;
+			dashCurrentTime = 0f;
+			dashDirection = Quaternion.Euler(0,-90,0) * transform.forward ;
+		}
+		if (rightDashCombo.Check ()) {
+			dash = true;
+			dashCurrentTime = 0f;
+			dashDirection = Quaternion.Euler(0,90,0) * transform.forward ;
+		}
+		if (frontDashCombo.Check ()) {
+			dash = true;
+			dashCurrentTime = 0f;
+			dashDirection = transform.forward;
+		}
+		if (backDashCombo.Check ()) {
+			dash = true;
+			dashCurrentTime = 0f;
+			dashDirection = -transform.forward;
+		}
+		
+		// Look
         transform.rotation = new Quaternion(0, player.Cam.transform.rotation.y, 0, player.Cam.transform.rotation.w);
+
+		// Animation
+		anim.SetFloat("Horizontal", movementInput.x);
+		anim.SetFloat("Vertical", movementInput.y);
+		
+//		if (!Input.GetKey(KeyCode.V)) {
+//			anim.SetFloat ("Horizontal", movementInput.x);
+//			anim.SetFloat ("Vertical", movementInput.y);
+//			anim.ResetTrigger("skill4");
+//		}else if(Input.GetKey(KeyCode.V)){
+//			if(Input.GetKey(KeyCode.B)){
+//				anim.SetTrigger("skill4");
+//			}else{
+//				anim.ResetTrigger("skill4");
+//			}
+//			anim.SetFloat ("Horizontal",2f);
+////			anim.SetFloat ("Vertical",2f);
+//		}else{
+//			anim.ResetTrigger("skill4");
+//		}
+
 	}
 
     void FixedUpdate() {
         if (player.Dead || gameManager.Win)
             return;
 		float currentSpeed = speed * slow;
-		Vector3 targetPosition = body.position + (((transform.forward * movementInput.y) + (transform.right * movementInput.x)) * currentSpeed);
+		
+		Vector3 targetPosition;
+		if (dash) {
+			currentSpeed *= dashSpeed;
+			targetPosition = body.position + dashDirection * currentSpeed;
+
+			dashCurrentTime += Time.fixedDeltaTime;
+			if (dashCurrentTime >= dashDuration) {
+				dash = false;
+				body.velocity = Vector3.zero;
+				body.angularVelocity = Vector3.zero;
+				// TODO: fix infinite movement after some collisions
+
+				return;
+			}
+
+		} else {
+			targetPosition = body.position + (((transform.forward * movementInput.y) + (transform.right * movementInput.x)) * currentSpeed);
+		}
+		
 		Vector3 smoothedTargetPosition = Vector3.SmoothDamp (body.position, targetPosition, ref currentVelocity, 0.2f, currentSpeed, Time.fixedDeltaTime);
 		body.MovePosition (smoothedTargetPosition);
 		if (jumpInput) {
