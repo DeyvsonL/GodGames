46a8df74b9040200fc38e69abcac61bfb78d720e
gaabs <gaabs@cin.ufpe.br>
2017-01-22 14:07:17 -0300

    Combo system refactor

--- Assets/Script/Player/ComboSystem.cs
+++ Assets/Script/Player/ComboSystem.cs
@@ -4,55 +4,57 @@ using UnityEngine;
 
 public class ComboSystem {
 
-	public KeyCode[]keys;
+	public KeyCode[] keys;
+	public float[] inBetweenTimes;
 	public int index;
 	public float inBetweenTime;
 	public float lastKeyPressTime;
 
-	public ComboSystem (KeyCode[] k)
+	public ComboSystem (KeyCode[] _k)
 	{
-		index=0;
-		inBetweenTime=0.5f;
-		lastKeyPressTime=0.0f;
-		keys=k;
+		index = 0;
+		lastKeyPressTime = 0.0f;
+		keys = _k;
+		inBetweenTimes = new float[_k.Length];
+		for (int i = 0; i < _k.Length; i++) {
+			inBetweenTimes [i] = 0.5f;
+		}
+	}
+
+	public ComboSystem (KeyCode[] _k, float inBetweenTime)
+	{
+		index = 0;
+		lastKeyPressTime = 0.0f;
+		keys = _k;
+		inBetweenTimes = new float[_k.Length];
+		for (int i = 0; i < _k.Length; i++) {
+			inBetweenTimes [i] = inBetweenTime;
+		}
+	}
+
+	public ComboSystem (KeyCode[] _k, float[] _inBetweenTimes)
+	{
+		inBetweenTimes = _inBetweenTimes;
+		index = 0;
+		lastKeyPressTime = 0.0f;
+		keys = _k;
 	}
 
 	public bool Check()
 	{
-		if ( Time.time > lastKeyPressTime+inBetweenTime)
-		{
-			index=0;
-			lastKeyPressTime=Time.time;
-			return false;
+		if (Time.time > lastKeyPressTime + inBetweenTimes[index]) {
+			index = 0;
+			lastKeyPressTime = Time.time;
+		} else if (Input.GetKeyDown (keys [index])) {
+			lastKeyPressTime = Time.time;
+			index++;
 		}
-		else
-		{
-			if(index<keys.Length)
-			{
-				if(Input.GetKeyDown(keys[index]))
-				{
-					lastKeyPressTime=Time.time;
-					index++;
-					if(index>=keys.Length)
-					{
-						index=0;
-						return true;
-					}
-					else
-					{
-						return false;
-					}
 
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else
-			{
-				return false;
-			}
+		if (index >= keys.Length) {
+			index = 0;
+			return true;
 		}
+
+		return false;
 	}
 }
